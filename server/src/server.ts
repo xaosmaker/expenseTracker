a5bf21957d47a4296ff51d023a7c510d58babe66
Drosos <drosinakis.drosos1@gmail.com>
2025-06-24 12:44:45 +0300

    create the basic folder structure

---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import express from "express"
+import { PORT } from "./settings.js"
+
+const server = express()
+
+
+
+
+
+
+server.get("/", (_req, res, _next) => {
+  res.json({ message: "Hello" })
+})
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+})
+
+
+
+
+
+

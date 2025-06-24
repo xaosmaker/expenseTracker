48236f9fe9c88c90ba0770e495dd035db243825c
Drosos <drososD@proton.me>
2025-06-24 15:25:28 +0300

    change the yaml to load .env and add an example.env

--- server/src/settings.ts
+++ server/src/settings.ts
@@ -1,1 +1,3 @@
 export const PORT = 8080
+export const CONN_STRING = `postgres://${process.env["POSTGRES_USER"]}:${process.env["POSTGRES_PASSWORD"]}@${process.env["POSTGRES_HOST"]}/${process.env["POSTGRES_DB"]}`
+

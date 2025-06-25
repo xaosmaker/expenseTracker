21b9f6a0b3e9587cb9fd5e3320e2177ef5485160
Drosos <drosinakis.drosos1@gmail.com>
2025-06-25 18:07:48 +0300

    create the user and add the errors to the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import express from "express"
 import { PORT } from "./settings.js"
+import { appErrorHandler } from "./errors/appErrorHandler.js"
+import { notFoundErrorHandler } from "./errors/notFoundErrorHandler.js"
+import { User } from "./types/User.js"
 
 const server = express()
 
@@ -12,18 +15,20 @@ const server = express()
 
 
 server.get("/", (_req, res, _next) => {
-  res.json({ message: "Hello" })
+  const date = new Date()
+  const user = new User({ id: 1, email: "somemail ", password: "1234", created_at: date, updated_at: date })
+  res.json(user.userWithoutPass())
 })
 
 
 
 
 
+//errors
+server.use(notFoundErrorHandler)
+server.use(appErrorHandler)
 
-
-
-
-
+//listen
 server.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 })

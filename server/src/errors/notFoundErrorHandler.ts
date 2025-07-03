3c9e3cafc284720803c7622f4a35c011373cdc8f
Drosos <drosinakis.drosos1@gmail.com>
2025-07-03 06:07:12 +0300

    fix typo AppErros to AppErrors and add field errors to json

--- server/src/errors/notFoundErrorHandler.ts
+++ server/src/errors/notFoundErrorHandler.ts
@@ -1,5 +1,5 @@
 import { Response, Request, NextFunction } from "express";
-import { AppError } from "./AppErros.js";
+import { AppError } from "./AppErrors.js";
 
 export function notFoundErrorHandler(_req: Request, _res: Response, _next: NextFunction) {
   throw new AppError("Page Not Found", 404)

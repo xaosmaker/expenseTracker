8a36f52d4a46830b2d67d22b940921165c8447a1
Drosos <drosinakis.drosos1@gmail.com>
2025-07-04 07:06:31 +0300

    create the PaymentType

---
+++ server/src/types/Payment.ts
@@ -0,0 +1,48 @@
+export interface CreatePayment {
+  name: string
+  payedDueDate: Date
+  amount: number
+  userId: number
+  isPayed: boolean
+}
+
+export interface CreatePaymentDB {
+  name: string
+  payed_due_date: Date
+  amount: number
+  user_id: number
+  is_payed: boolean
+
+}
+
+
+
+export interface PaymentDB extends CreatePaymentDB {
+  id: number
+  created_at: Date
+  updated_at: Date
+}
+
+
+export class Payment {
+  id: number
+  name: string
+  payedDueDate: Date
+  amount: number
+  userId: number
+  isPayed: boolean
+  createdAt: Date
+  updatedAt: Date
+  constructor({ id, name, payed_due_date, amount, user_id, is_payed, created_at, updated_at }: PaymentDB) {
+    this.id = id
+    this.name = name
+    this.payedDueDate = payed_due_date
+    this.amount = amount
+    this.userId = user_id
+    this.isPayed = is_payed
+    this.createdAt = created_at
+    this.updatedAt = updated_at
+  }
+
+
+}

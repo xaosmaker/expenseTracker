defff39b8b124595ee2944c975eb69bed5566bfa
Drosos <drosinakis.drosos1@gmail.com>
2025-07-04 08:07:51 +0300

    finish the api createPayment

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import { appErrorHandler } from "./errors/appErrorHandler.js"
 import { notFoundErrorHandler } from "./errors/notFoundErrorHandler.js"
 import { userRoutes } from "./routes/userRoutes.js"
 import { authMiddleware } from "./middlewares/authMiddleware.js"
+import { isAuthenticatedMiddleware } from "./middlewares/isAuthenticatedMiddleware.js"
+import { paymentRoutes } from "./routes/paymentRoutes.js"
 
 const server = express()
 
@@ -14,6 +16,7 @@ server.use(authMiddleware)
 
 
 server.use("/api/users", userRoutes)
+server.use("/api/payments", isAuthenticatedMiddleware, paymentRoutes)
 
 
 

4b45f32bf36b71bd57b6e7249a47dba58db23235
Drosos <drosinakis.drosos1@gmail.com>
2025-07-10 19:21:19 +0300

    create paymentTable

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,8 +1,8 @@
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-// import Typography from "@mui/material/Typography";
 import { useContext } from "react";
 import { LayoutContext } from "./Layout";
+import { Outlet } from "react-router-dom";
 
 export default function Main() {
 
@@ -18,13 +18,11 @@ export default function Main() {
         flexGrow: 1,
         p: 3,
         width: { md: `calc(100% - ${layoutContext.drawerWidth}px)` },
+        overflowX: "hidden"
       }}
     >
       <Toolbar />
-      <div className="h-dvh bg-red-50">
-
-        Hello from here
-      </div>
+      <Outlet />
     </Box>
   )
 }
--- client/src/router/baseRoutes.ts
+++ client/src/router/baseRoutes.ts
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from "react-router-dom"
 import LoginReg from "../users/routes/LoginReg"
-import Payments from "../payments/Payments"
 import Layout from "../components/Layout"
+import PaymentTable from "../payments/routes/PaymentTable"
 
 
 export const router = createBrowserRouter([
   { path: "/login", Component: LoginReg },
   { path: "/register", Component: LoginReg },
-  { path: "/payments", Component: Payments },
-  { path: "/", Component: Layout }
+  {
+    path: "/", Component: Layout, children: [
+      { path: "/payments", Component: PaymentTable },
+
+    ]
+  },
+
 
 ])

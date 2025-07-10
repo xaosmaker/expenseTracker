3c02a4d52a26de97187a2e1d186c556e394208a3
Drosos <drososD@proton.me>
2025-07-10 20:13:05 +0300

    make the base auth for protected routes

---
+++ client/src/auth/routes/RequireAuth.tsx
@@ -0,0 +1,35 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { parse } from "cookie";
+
+export default function RequireAuth() {
+  const cookie = parse(document.cookie)
+  let loginCookie: { isLogin: boolean } = { isLogin: false };
+
+  if (typeof cookie.login === "string") {
+    try {
+      loginCookie = JSON.parse(cookie.login);
+    }
+    catch (e) {
+      console.log(e);
+    }
+
+  }
+  console.log(loginCookie.isLogin);
+
+  if (loginCookie.isLogin) {
+    return <Outlet />
+
+  }
+  return <Navigate to={"/login"}></Navigate>
+
+
+
+
+
+
+
+
+
+
+}
+
--- client/src/router/baseRoutes.ts
+++ client/src/router/baseRoutes.ts
@@ -2,17 +2,23 @@ import { createBrowserRouter } from "react-router-dom"
 import LoginReg from "../users/routes/LoginReg"
 import Layout from "../components/Layout"
 import PaymentTable from "../payments/routes/PaymentTable"
+import RequireAuth from "../auth/routes/RequireAuth"
 
 
 export const router = createBrowserRouter([
   { path: "/login", Component: LoginReg },
   { path: "/register", Component: LoginReg },
   {
-    path: "/", Component: Layout, children: [
-      { path: "/payments", Component: PaymentTable },
+    Component: RequireAuth, children: [
+      {
+        path: "/", Component: Layout, children: [
+          { path: "/payments", Component: PaymentTable },
+
+        ]
+      },
 
     ]
-  },
+  }
 
 
 ])

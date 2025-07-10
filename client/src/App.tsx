86fe954fddeba930f425c3c194b4105fbf54bc80
Drosos <drosinakis.drosos1@gmail.com>
2025-07-10 16:12:57 +0300

    make the basic layout with sibebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router/baseRoutes"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 function App() {
 
-  return <RouterProvider router={router} />
+  const queryClient = new QueryClient()
+
+  return (
+
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 
 
 }
--- client/src/router/baseRoutes.ts
+++ client/src/router/baseRoutes.ts
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom"
 import LoginReg from "../users/routes/LoginReg"
+import Payments from "../payments/Payments"
+import Layout from "../components/Layout"
 
 
 export const router = createBrowserRouter([
   { path: "/login", Component: LoginReg },
-  { path: "/register", Component: LoginReg }
+  { path: "/register", Component: LoginReg },
+  { path: "/payments", Component: Payments },
+  { path: "/", Component: Layout }
+
 ])

c7255c212bb563a79beebec042a2a75d8449bb45
Drosos <drososD@proton.me>
2025-07-15 03:19:44 +0300

    rename auth to athentication

--- client/src/router/baseRoutes.ts
+++ client/src/router/baseRoutes.ts
@@ -2,9 +2,9 @@ import { createBrowserRouter } from "react-router-dom"
 import LoginReg from "../users/routes/LoginReg"
 import Layout from "../components/Layout"
 import PaymentTable from "../payments/pages/PaymentTable"
-import RequireAuth from "../auth/routes/RequireAuth"
 import CreatePayment from "../payments/pages/CreatePayment"
 import { DeleteModal } from "../components/DeleteModal"
+import RequireAuth from "../authentication/pages/RequireAuth"
 
 
 export const router = createBrowserRouter([

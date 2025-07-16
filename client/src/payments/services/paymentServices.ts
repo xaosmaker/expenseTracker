065d4061d4e39bf633f02d922520944b41af75cd
Drosos <drososD@proton.me>
2025-07-16 11:26:22 +0300

    create The payment Form

--- client/src/payments/services/paymentServices.ts
+++ client/src/payments/services/paymentServices.ts
@@ -1,8 +1,26 @@
+import { AxiosError } from "axios";
 import { api } from "../../services/api";
+import type { CreatePayment } from "../types/paymentTypes";
 
 export async function getPayments() {
   const res = await api.get("/api/payments/")
   const data = await res.data
   return data
 
+}
+
+export async function createPayment(data: CreatePayment) {
+  try {
+
+    const res = await api.post("/api/payments", data)
+    const resData = await res.data
+    return resData
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      console.log("createPayment axion instance:", e);
+      throw new Error("Something went wrong")
+    }
+    console.log("createPayment: general error", e);
+
+  }
 }
---
+++ client/src/payments/types/paymentTypes.ts
@@ -0,0 +1,20 @@
+import { z } from "zod/v4"
+export interface CreatePayment {
+  name: string
+  payedDueDate: string
+  amount: number
+  isPayed: boolean
+}
+
+
+
+
+export const paymentSchema = z.object({
+  name: z.string().min(2, "This Field is Required"),
+  payedDueDate: z.coerce.date<Date>('Payed Due Date Required'),
+  amount: z.coerce.number<number>().nonnegative().min(0.001, "Amount Required"),
+  isPayed: z.coerce.boolean<boolean>()
+}
+)
+
+export type PaymentSchema = z.infer<typeof paymentSchema>
